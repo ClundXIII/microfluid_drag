65d1d172fe75651814d43d3683ee5bbfab600c4f
Simon Michalke <simon.m.m@freenet.de>
2016-08-09 14:15:45 +0200

    ...

---
+++ src/define.h
@@ -0,0 +1,6 @@
+#ifndef DEFINE_H_INCLUDED
+#define DEFINE_H_INCLUDED
+
+#define _USE_VEMC2 0
+
+#endif // DEFINE_H_INCLUDED
---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+#include "define.h"
+
+#if ( _USE_VEMC2 == 1 )
+#include <vemc2/core/universe.h>
+#endif // _USE_VEMC2
+
+int main(){
+
+    std::cout << "Loading Lattice Boltzmann ..." << std::endl;
+    std::cout << "              (c) Simon Michalke, 2016" << std::endl;
+
+    #if ( _USE_VEMC2 == 1 )
+        vemc2::universe u(-1);
+        u.out << "Hi" << Vesper::LoggingTypes::eom;
+    #endif // _USE_VEMC2
+
+
+    std::cout << "exit(0);" << std::endl;
+    return 0;
+}
