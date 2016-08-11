8c8e4d46ec3224fc823fb6173af7d22c7f41f951
Simon Michalke <simon.m.m@freenet.de>
2016-08-12 00:21:28 +0200

    added size statics

--- src/define.h
+++ src/define.h
@@ -5,24 +5,29 @@
 
 enum direction{
     _000 =  0,
-    _p00 =  7,
+    _p00 =  1,
     _m00 =  2,
     _0p0 =  3,
     _0m0 =  4,
     _00p =  5,
     _00m =  6,
-    _pp0 =  8,
-    _pm0 =  9,
-    _p0p = 10,
-    _p0m = 11,
-    _mp0 = 13,
-    _mm0 = 14,
-    _m0p = 15,
-    _m0m = 16,
-    _0pp = 17,
-    _0pm = 18,
-    _0mp = 19,
-    _0mm = 20
+
+    //
+    _pp0 =  7,
+    _pm0 =  8,
+    _p0p =  9,
+    _p0m = 10,
+    _mp0 = 11,
+    _mm0 = 12,
+    _m0p = 13,
+    _m0m = 14,
+    _0pp = 15,
+    _0pm = 16,
+    _0mp = 17,
+    _0mm = 18
 };
+
+#define _size_D3Q6   7
+#define _size_D3Q18 19
 
 #endif // DEFINE_H_INCLUDED
